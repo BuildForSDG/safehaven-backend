bb16d4b84363b73221008c45f62f8ec3962c1d1f
Lovisgod <olifedayo94@gmail.com>
2020-05-06 21:47:28 +0100

    feature: created models and migrations for the user table && created authcontroller file

---
+++ src/controllers/AuthController.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-unused-vars */
+import token from 'uuid';
+import model from '../models';
+import {
+  validate, inValidName, inValidEmail, inValidPassword, magicTrimmer
+} from '../utils/validator';
+import { sendErrorResponse, sendSuccessResponse } from '../utils/sendResponse';
+import { hashPassword, comparePassword } from '../utils/passwordHash';
+import { SendMail, sendForgotPasswordMail } from '../services/emailsender';
+import { createToken } from '../utils/processToken';
+
+const { User } = model;
+
+// returns a token for logged in user or during sign up
+const userToken = (user) => {
+  const {
+    email, name, role, uuid
+  } = user;
+  return {
+    token: createToken({
+      uuid,
+      name,
+      email,
+      role
+    })
+  };
+};
+
+const AuthController = {
+  async signin(req, res, next) {
+    try {
+      // extracting user data
+      const { email, password, username } = req.body;
+
+      // checking if the user exist
+      const user = email ? await User.findOne({ where: { email } })
+        : await User.findOne({ where: { username } });
+
+      if (!user) return sendErrorResponse(res, 404, 'User Not Found!!');
+
+      // compare password
+      const checkPassword = comparePassword(password, user.dataValues.password);
+      if (!checkPassword) return sendErrorResponse(res, 400, 'Incorrect Password');
+
+      // check user verification
+      if (!user.dataValues.verified) return sendErrorResponse(res, 401, 'Verify Your Account ');
+      const createdToken = userToken(user.dataValues);
+
+      return sendSuccessResponse(res, 200, { token: createdToken });
+    } catch (e) {
+      return next(e);
+    }
+  }
+};
+
+export default AuthController;
---
+++ src/models/User.js
@@ -0,0 +1,63 @@
+/* eslint-disable no-unused-vars */
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      uuid: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+      },
+      firstName: DataTypes.STRING,
+      surname: DataTypes.STRING,
+      middleName: DataTypes.STRING,
+      email: {
+        type: DataTypes.STRING,
+        allowNull: {
+          args: false,
+          msg: 'Please enter your Email Address'
+        },
+        unique: {
+          args: true,
+          msg: 'Email Already Exist!!!'
+        },
+        validate: {
+          isEmail: {
+            args: true,
+            msg: 'Please Enter A Valid Email Address'
+          }
+        }
+      },
+      phone: DataTypes.REAL,
+      gender: DataTypes.STRING,
+      dateOfBirth: DataTypes.STRING,
+      nationality: DataTypes.STRING,
+      avatar: DataTypes.STRING,
+      stateOfOrigin: DataTypes.STRING,
+      password: DataTypes.STRING,
+      address: DataTypes.STRING,
+      verified: DataTypes.BOOLEAN,
+      role: {
+        type: DataTypes.ENUM('user', 'admin', 'consultant'),
+        defaultValue: 'user'
+      }
+    },
+    {}
+  );
+  User.associate = function (models) {
+    // associations can be defined here
+    // User.hasMany(models.Token, {
+    //   foreignKey: 'user_uuid',
+    //   as: 'tokens',
+    //   onDelete: 'CASADE'
+    // });
+
+    // User.hasMany(models.Profile, {
+    //   foreignKey: 'user_uuid',
+    //   as: 'profiles',
+    //   onDelete: 'CASADE'
+    // });
+  };
+  return User;
+};
