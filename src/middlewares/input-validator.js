70da9538a326d611cd002f4887cad3b8f4fe0a1a
john akins <johnakins.dev@gmail.com>
2020-05-11 17:57:22 +0100

    feature(signup) optimize request validation with database query function

--- src/middlewares/input-validator.js
+++ src/middlewares/input-validator.js
@@ -3,19 +3,33 @@ import {
   body
 } from 'express-validator';
 
+import authValidator from '../database/input-validator/auth';
+
 const validator = {};
 
 validator.userSignup = [
   body('firstName').not().isEmpty().isLength({ min: 2 }),
   body('surName').not().isEmpty().isLength({ min: 2 }),
-  body('middleName').not().isEmpty().isLength({ min: 2 }),
-  body('email').normalizeEmail().isEmail(),
-  body('phone').not().isEmpty().isLength({ min: 8 })
-    .isNumeric(),
+  body('middleName'),
   body('password').not().isEmpty().isLength({ min: 8 }),
   body('gender').isIn('male', 'female', 'not specified'),
   body('role', 'invalid user role').isIn('patient', 'admin', 'consultant'),
-  body('conditions').not().isEmpty()
+  body('conditions').not().isEmpty(),
+  body('phone').not().isEmpty().isLength({ min: 8 })
+    .isNumeric()
+    .custom((phone) => authValidator.numberIsUnique(phone)
+      .then((numberIsUnique) => {
+        if (numberIsUnique) {
+          return Promise.reject(Error('Phone already in use'));
+        }
+      })),
+  body('email').normalizeEmail().isEmail()
+    .custom((email) => authValidator.emailIsUnique(email)
+      .then((emailIsUnique) => {
+        if (emailIsUnique) {
+          return Promise.reject(Error('E-mail already in use'));
+        }
+      }))
 ];
 
 validator.login = [
