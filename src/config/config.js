07dd952a7a112a375e25e1434a04bc7374be4de6
adaezeodurukwe <daizyodurukwe@gmail.com>
2020-05-19 16:25:25 +0100

    remove unwnanted code

--- src/config/config.js
+++ src/config/config.js
@@ -7,10 +7,7 @@ module.exports = {
   development: {
     use_env_variable: 'DATABASE_URL_DEV',
     dialect: 'postgres',
-    logging: false,
-    dialectOptions: {
-      ssl: true
-    }
+    logging: false
   },
   test: {
     use_env_variable: 'DATABASE_URL_TEST',
