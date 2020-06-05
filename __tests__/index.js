93489f03ffcc31c378162568c7d44b3b6f287328
Lovisgod <olifedayo94@gmail.com>
2020-06-05 15:32:22 +0100

    test: travis ci not running all tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -3,10 +3,12 @@ import './unit/server.test';
 import './unit/process-token.test';
 import './unit/dateChecker.test';
 import './unit/mailer.test';
-
-// // Integration test
 import './integration/auth.test';
 import './integration/signup.test';
+
+// Integration test
+// import './integration/auth.test';
+// import './integration/signup.test';
 import './integration/signup-consultant.test';
 import './integration/get-user-profile.test';
 import './integration/consultant-profile-update.test';
