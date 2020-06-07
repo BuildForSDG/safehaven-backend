34cb55a777ac3059faf4a03958d8c2d2e707cd6e
Lovisgod <olifedayo94@gmail.com>
2020-06-07 10:30:26 +0100

    chore: remove unneccasry loggin

--- src/socket/index.js
+++ src/socket/index.js
@@ -4,13 +4,11 @@ import connection from './connection';
 export default (io) => {
   if (io) {
     io.on('connection', async (socket) => {
-      console.log('a user connected');
       let user;
       const { handshake } = socket;
       socket.on('authenticate', async (data) => {
         try {
           const header = data.token;
-          console.log(header);
           user = await SocketAuth(header, socket, io);
         } catch (error) {
           console.log(error);
@@ -20,7 +18,6 @@ export default (io) => {
         const header = handshake.headers.token;
         if (header) {
           user = await SocketAuth(header, socket, io);
-          console.log(user);
         }
       }
       // handle other processes here
