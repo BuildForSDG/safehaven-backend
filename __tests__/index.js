f099b5d6ee5233bff4d791a4fd39bab71843e5d3
john akins <johnakins.dev@gmail.com>
2020-06-17 00:32:00 +0100

    uncomments tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,15 +1,12 @@
-/*
 // Unit test
 import './unit/server.test';
 import './unit/process-token.test';
 import './unit/dateChecker.test';
 import './unit/mailer.test';
 import './integration/get-user-profile.test';
-*/
 
 // Integration test
 import './integration/appointments.test';
-/*
 import './integration/auth.test';
 import './integration/signup.test';
 import './integration/signup-consultant.test';
@@ -17,4 +14,3 @@ import './integration/consultant-profile-update.test';
 import './integration/patient-profile-update.test';
 import './integration/socket.test';
 import './integration/admin.test';
-*/
